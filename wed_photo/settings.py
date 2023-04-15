701ee39da092167d442791b8c6ad8db647314f78
ivsolow <116672920+ivsolow@users.noreply.github.com>
2023-04-15 18:15:23 +0000

    Update settings.py

--- wed_photo/settings.py
+++ wed_photo/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os
 from pathlib import Path
-from dotenv import load_dotenv
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = '9fn@l(r7hhm&60odrdli=c!105n=8oilh@=5loy#k5xf51x12d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
